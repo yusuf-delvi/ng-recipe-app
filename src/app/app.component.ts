f589b91ce7678b4fd712af8c0b0e2acc306a116c
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-05-20 19:38:02 +0530

    routing using ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-recipe-app';
+  pageValue: string = 'recipes';
+
+  pageVal(value: string) {
+    this.pageValue = value;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   public isMenuCollapsed = true;
   public isManageCollapsed = true;
+
+  @Output() navToVal = new EventEmitter<string>();
+
+  navTo(val: string) {
+    this.navToVal.emit(val);
+  }
 }

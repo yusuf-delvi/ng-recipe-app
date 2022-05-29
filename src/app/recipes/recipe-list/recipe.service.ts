8833a6fdf13dddb7e43ef2ff9940f303a4ff1764
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-05-30 00:03:43 +0530

    init recipe service

---
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -0,0 +1,22 @@
+import { Recipe } from '../recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'A test recipe 1',
+      'This is simply a test 1',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  constructor() {}
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+
+import { RecipeService } from './recipe-list/recipe.service';
+
 import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
+  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
   constructor() {}

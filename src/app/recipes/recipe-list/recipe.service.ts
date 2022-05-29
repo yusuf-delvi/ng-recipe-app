1853c57fe43d0face3d22879697b941ce66becb8
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-05-30 00:59:06 +0530

    add ingredients to recipe list

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 import { Recipe } from '../recipe.model';
 
 export class RecipeService {
@@ -8,12 +10,14 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A test recipe 1',
       'This is simply a test 1',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 

afc7b8544171b2421f502f528a1cffbc83bda4b1
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-07-05 07:57:02 +0530

    new comp recipe edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeEditComponent,
   ],
   imports: [BrowserModule, NgbModule, AppRoutingModule],
   providers: [ShoppingListService],
